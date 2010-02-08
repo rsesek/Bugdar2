3cfe93419b2f83fa97b88b8e7bdf49675b94ab35
Robert Sesek <rsesek@bluestatic.org>
2010-02-08 00:07:49 -0500

    Add BugList event

---
+++ events/bug_list.php
@@ -0,0 +1,51 @@
+<?php
+// Bugdar 2
+// Copyright (c) 2010 Blue Static
+// 
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use phalanx\events\EventPump as EventPump;
+
+// This generates a list of bugs.
+class BugListEvent extends phalanx\events\Event
+{
+    // The list of bugs we compiled.
+    protected $bugs = array();
+    public function bugs() { return $this->bugs; }
+
+    static public function InputList()
+    {
+        // Nothing now. Future work will be search params, filters, view layout
+        // info, etc.
+        return array();
+    }
+
+    static public function OutputList()
+    {
+        return array('bugs');
+    }
+
+    public function Fire()
+    {
+        $query = Bugdar::$db->Query("
+            SELECT bugs.*, users.alias as reporting_alias
+            FROM " . TABLE_PREFIX . "bugs bugs
+            LEFT JOIN " . TABLE_PREFIX . "users users
+                ON (bugs.reporting_user_id = users.user_id)
+            ORDER BY reporting_date
+            LIMIT 30
+        ");
+        while ($bug = $query->FetchObject())
+            $this->bugs[] = $bug;
+    }
+}
---
+++ templates/bug_list.tpl
@@ -0,0 +1,19 @@
+<h1>Top 30 Recent Bugs</h1>
+<p>(This will get better, I assure you.)</p>
+
+<table border="1" cellspacing="2" cellpadding="5">
+    <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Reporter</th>
+        <th>Date</th>
+    </tr>
+    <? foreach ($this->bugs as $bug): ?>
+    <tr>
+        <td><a href="view_bug/<?= Cleaner::Int($bug->bug_id) ?>"><?= Cleaner::HTML($bug->bug_id) ?></a></td>
+        <td><a href="view_bug/<?= Cleaner::Int($bug->bug_id) ?>"><?= Cleaner::HTML($bug->title) ?></a></td>
+        <td><a href="view_user/<?= Cleaner::HTML($bug->reporting_alias)?>"><?= Cleaner::HTML($bug->reporting_alias) ?></a></td>
+        <td><?= gmdate('r', $bug->reporting_date + (Bugdar::$auth->current_user()->timezone * 3600)) ?></td>
+    </tr>
+    <? endforeach ?>
+</table>
