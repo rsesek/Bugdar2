2b7b6274fe1b2545e41b864377623c1b3afb7552
Robert Sesek <rsesek@bluestatic.org>
2010-02-11 20:22:58 -0500

    Add the BugViewEvent

---
+++ events/bug_view.php
@@ -0,0 +1,67 @@
+<?php
+// Bugdar 2
+// Copyright (c) 2010 Blue Static
+// 
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use phalanx\events\EventPump as EventPump;
+
+// This views information of a bug.
+class BugViewEvent extends phalanx\events\Event
+{
+    // The full bug object.
+    protected $bug = NULL;
+    public function bug() { return $this->bug; }
+
+    // Array of comments. Oldest to newest.
+    protected $comments = array();
+    public function comments() { return $this->comments; }
+
+    static public function InputList()
+    {
+        return array('_id');
+    }
+
+    static public function OutputList()
+    {
+        return array(
+            'bug',
+            'comments'
+        );
+    }
+
+    public function Fire()
+    {
+        $stmt = Bugdar::$db->Prepare("
+            SELECT bugs.*, users.alias as reporting_alias
+            FROM " . TABLE_PREFIX . "bugs bugs
+            LEFT JOIN " . TABLE_PREFIX . "users users
+                ON (bugs.reporting_user_id = users.user_id)
+            WHERE bugs.bug_id = ?
+        ");
+        $stmt->Execute(array($this->input->_id));
+        $this->bug = $stmt->FetchObject();
+
+        $stmt = Bugdar::$db->Prepare("
+            SELECT comments.*, users.alias as post_alias
+            FROM " . TABLE_PREFIX . "comments comments
+            LEFT JOIN " . TABLE_PREFIX . "users users
+                ON (comments.post_user_id = users.user_id)
+            WHERE comments.bug_id = ?
+            ORDER BY comments.post_date
+        ");
+        $stmt->Execute(array($this->input->_id));
+        while ($comment = $stmt->FetchObject())
+            $this->comments[] = $comment;
+    }
+}
---
+++ templates/bug_view.tpl
@@ -0,0 +1,25 @@
+<h1>Bug #$[bug.bug_id]: $[bug.title]</h1>
+
+<dl>
+    <dt>Reporter:</dt>
+    <dd>$[bug.reporting_alias]</dd>
+
+    <dt>Date:</dt>
+    <dd><?= gmdate('r', $this->bug->reporting_date + (Bugdar::$auth->current_user()->timezone * 3600)) ?></dd>
+</dl>
+
+<h2>Comments</h2>
+
+<? foreach ($this->comments as $comment): ?>
+<div>
+    <strong>Posted by <a href="view_user/<?= Cleaner::HTML($comment->post_alias) ?>"><?= Cleaner::HTML($comment->post_alias) ?></a> on <?= gmdate('r', $comment->post_date + (Bugdar::$auth->current_user()->timezone * 3600)) ?></strong>
+    <p><?= Cleaner::HTML($comment->body) ?></p>
+</div>
+<? endforeach ?>
+
+<form action="new_comment" method="post">
+    <input type="hidden" name="bug_id" value="$[bug.bug_id]" />
+    <input type="hidden" name="do" value="submit" />
+    <textarea name="body" rows="8" cols="40"></textarea>
+    <input type="submit" name="submit" value="Add Comment" id="submit" />
+</form>
